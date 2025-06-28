4c54127add36c169d167722abf7ec0c05476045c
eleadn <eleano.dn@gmail.com>
2025-06-28 21:07:09 +0200

    Add project files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,18 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+    },
+  },
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
